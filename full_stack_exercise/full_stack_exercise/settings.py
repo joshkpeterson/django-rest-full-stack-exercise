beec40565ed21cd9c0baa4dff3c1be468d074ba9
Josh <joshkpete@gmail.com>
2020-08-23 23:26:01 -0400

    Add postgres settings

--- full_stack_exercise/full_stack_exercise/settings.py
+++ full_stack_exercise/full_stack_exercise/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'full_stack_exercise.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fullstack_db',
+        'USER': 'tester',
+        'PASSWORD': 'test_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
