da7956dd3b89bc10bce23b295752a2ce9d6c5274
Josh Peterson <joshkpete@gmail.com>
2020-08-24 12:47:40 -0400

    Feature/jp/backend scaffolding (#8)
    * Add starter gitignore
    
    * Create full_stack_exercise app
    
    * Add uploads_app
    
    * Add postgres settings
    
    * Add authentication app
    
    * Add starter custom user model
    
    * Add localflavor and USSocialSecurityNumberField
    
    * Fix project structure and do migrations

---
+++ full_stack_exercise/authentication/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from localflavor.us.forms import USSocialSecurityNumberField
+
+
+class Role(models.Model):
+    USER = 1
+    ADMIN = 2
+    ROLE_CHOICES = (
+        (USER, 'user'),
+        (ADMIN, 'admin'),
+    )
+
+    id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
+
+    def __str__(self):
+        return self.get_id_display()
+
+
+class CustomUser(AbstractUser):
+    SSN = USSocialSecurityNumberField()
+    roles = models.ManyToManyField(Role)
