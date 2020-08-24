652e56f25ae099e045ed26cade98d0d70d5c26fd
Josh Peterson <joshkpete@gmail.com>
2020-08-24 18:08:25 -0400

    Feature/jp/authentication (#9)
    * Add DRF and Simple JWT config to settings
    
    * Add urls for simple jwt token obtain and refresh
    
    * Add starter CustomUserSerializer
    
    * Add CustomUserCreate view
    
    * Add user create to urls
    
    * Change user roles field to role
    
    * Adjust SSN field type and validation
    
    * Add hello world view and url
    
    * Clean up Role model and change role to CharField

--- full_stack_exercise/authentication/models.py
+++ full_stack_exercise/authentication/models.py
@@ -3,20 +3,11 @@ from django.db import models
 from localflavor.us.forms import USSocialSecurityNumberField
 
 
-class Role(models.Model):
-    USER = 1
-    ADMIN = 2
-    ROLE_CHOICES = (
-        (USER, 'user'),
-        (ADMIN, 'admin'),
-    )
-
-    id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
-
-    def __str__(self):
-        return self.get_id_display()
-
-
 class CustomUser(AbstractUser):
-    SSN = USSocialSecurityNumberField()
-    roles = models.ManyToManyField(Role)
+
+    class UserRoles(models.TextChoices):
+        USER = 'user'
+        ADMIN = 'admin'
+
+    role = models.CharField(choices=UserRoles.choices, max_length=64)
+    SSN = models.CharField(max_length=11, validators = [USSocialSecurityNumberField.clean])
---
+++ full_stack_exercise/authentication/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(min_length=8, write_only=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    role = serializers.ChoiceField(choices=CustomUser.UserRoles, required=True)
+    SSN = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password', 'first_name', 'last_name', 'role', 'SSN')
+        extra_kwargs = {'password': {'write_only': True}, 'SSN': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)  # as long as the fields are the same, we can just use this
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+    # def get_token(self, object):
+    #     jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+    #     jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+    #     payload = jwt_payload_handler(object)
+    #     token = jwt_encode_handler(payload)
+    #     return token
